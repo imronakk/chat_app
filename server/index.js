d4042c5f56ab817d954546ebaa927a3e5c3a0401
Ronak Deshpande <ronakcode11@gmail.com>
2023-09-24 09:28:36 +0530

    Server progress

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB is connected successfully");
+}).catch((e)=>{
+    console.error(`Error connecting to MongoDB Atlas cluster - ${e.message}`);
+});
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is connected to PORT: ${process.env.PORT}`);
+});
